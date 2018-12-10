3121ff87033051908a25754443d04e429cff9b8d
Seva Zaikov <seva.zaikov@gmail.com>
2018-12-10 00:10:15 -0800

    add solution to the first part of 5th task

---
+++ 5.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+const originalPolymer = fs
+  .readFileSync("5_input.txt", { encoding: "utf-8" })
+  .trim();
+let polymer = originalPolymer;
+
+let position = 0;
+
+while (true) {
+  if (position === polymer.length - 1) {
+    break;
+  }
+
+  const elem = polymer[position];
+  const nextElem = polymer[position + 1];
+
+  if (makeReaction(elem, nextElem)) {
+    const firstPart = polymer.substring(0, position);
+    const secondPart = polymer.substring(position + 2, polymer.length);
+    polymer = firstPart + secondPart;
+
+    if (position > 0) {
+      position--;
+    }
+  } else {
+    position++;
+  }
+}
+
+console.log(`Final length of reaction is ${polymer.length} symbols.`);
+console.log(`Original length was ${originalPolymer.length} symbols`);
+
+function makeReaction(el1, el2) {
+  return el1 !== el2 && el1.toLowerCase() === el2.toLowerCase();
+}
