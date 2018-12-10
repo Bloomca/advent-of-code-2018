5c85724d981a2e37d134425bc7ddbd55f5944346
Seva Zaikov <seva.zaikov@gmail.com>
2018-12-10 03:52:40 -0800

    add solution to the first part of 7th task

---
+++ 7.js
@@ -0,0 +1,79 @@
+const fs = require("fs");
+
+const data = fs
+  .readFileSync("7_input.txt", { encoding: "utf-8" })
+  .trim()
+  .split("\n");
+
+const graphData = data.map(str => {
+  const [_full, currentStep, nextStep] = str.match(
+    /^Step ([A-Z]).+?step ([A-Z])/
+  );
+
+  return { currentStep, nextStep };
+});
+
+const nodes = {};
+const locks = {};
+
+let steps = [];
+const nextSteps = {};
+
+graphData.forEach(({ currentStep, nextStep }) => {
+  nextSteps[nextStep] = true;
+  if (!nodes[currentStep]) {
+    nodes[currentStep] = [];
+  }
+
+  if (!steps.includes(currentStep) && !nextSteps[currentStep]) {
+    steps.push(currentStep);
+  }
+
+  if (steps.includes(nextStep)) {
+    steps = steps.filter(step => step !== nextStep);
+  }
+
+  nodes[currentStep].push(nextStep);
+
+  if (!locks[nextStep]) {
+    locks[nextStep] = [];
+  }
+
+  locks[nextStep].push(currentStep);
+});
+
+steps.sort();
+
+let path = steps.shift();
+let options = steps;
+options.sort();
+const covered = { [path]: true };
+
+while (true) {
+  if (options.length === 0) {
+    break;
+  }
+
+  let nextStep;
+  while (true) {
+    const step = options.shift();
+
+    const unlocked = (locks[step] || []).every(x => covered[x]);
+
+    if (unlocked) {
+      nextStep = step;
+      break;
+    } else {
+      options.push(step);
+    }
+  }
+
+  path += nextStep;
+
+  covered[nextStep] = true;
+
+  options = options.concat(nodes[nextStep] || []).filter(x => !covered[x]);
+  options.sort();
+}
+
+console.log(path);
