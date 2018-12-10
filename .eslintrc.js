c4f0f1becb1e09b35a06a7492faaddf6537a572f
Seva Zaikov <seva.zaikov@gmail.com>
2018-12-09 21:56:47 -0800

    loosen up eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
     "no-continue": 0,
     "comma-dangle": 0,
     "no-use-before-define": 0,
-    "arrow-parens": 0
+    "arrow-parens": 0,
+    "prefer-destructuring": 0,
+    "no-param-reassign": 0
   }
 };
