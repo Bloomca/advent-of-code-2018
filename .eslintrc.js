0456541d4b249495db66287efc16346ac38f7afb
Seva Zaikov <seva.zaikov@gmail.com>
2018-12-09 17:51:59 -0800

    improve eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   extends: "airbnb-base",
   rules: {
     quotes: ["error", "double", { avoidEscape: true }],
-    "no-plusplus": 0
+    "no-plusplus": 0,
+    "no-continue": 0,
+    "comma-dangle": 0,
+    "no-use-before-define": 0
   }
 };
