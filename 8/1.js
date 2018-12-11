f660263aa0cddf8e2b7c3dc6b5d42a346fc4e96d
Seva Zaikov <seva.zaikov@gmail.com>
2018-12-10 22:39:24 -0800

    restore 1st part of 8th problem

--- 8/1.js
+++ 8/1.js
@@ -1,40 +1,27 @@
 const fs = require("fs");
 
 const data = fs
-  .readFileSync("input.txt", { encoding: "utf-8" })
+  .readFileSync("8_input.txt", { encoding: "utf-8" })
   .trim()
   .split(" ")
   .map(Number);
+
+let sum = 0;
 
 function parseNodes(nodes) {
   const [childrenNumber, metadataNumber, ...rest] = nodes;
 
   let dataWithoutChildren = rest;
-  const results = [];
   for (let i = 0; i < childrenNumber; i++) {
-    const [childSum, restNodes] = parseNodes(dataWithoutChildren);
-    results.push(childSum);
-    dataWithoutChildren = restNodes;
+    dataWithoutChildren = parseNodes(dataWithoutChildren);
   }
 
-  const restNodes = dataWithoutChildren.slice(metadataNumber);
-  if (childrenNumber === 0) {
-    const sum = dataWithoutChildren
-      .slice(0, metadataNumber)
-      .reduce((acc, x) => acc + x, 0);
-
-    return [sum, restNodes];
-  }
-
-  const sum = dataWithoutChildren
-    .slice(0, metadataNumber)
-    .map(index => results[index])
-    .map(Boolean)
-    .reduce((acc, x) => acc + x, 0);
-
-  return [sum, restNodes];
+  dataWithoutChildren.slice(0, metadataNumber).forEach(x => {
+    sum += x;
+  });
+  return dataWithoutChildren.slice(metadataNumber);
 }
 
-const result = parseNodes(data);
+parseNodes(data);
 
-console.log(result);
+console.log(sum);
